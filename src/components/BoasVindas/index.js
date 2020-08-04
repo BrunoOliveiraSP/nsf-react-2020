37d53d29a2d14ae2b5b14be9d4c8652330e08ef7
Bruno de Oliveira <brunooliveira.me@outlook.com>
2020-08-04 12:59:50 -0300

    okkk

---
+++ src/components/BoasVindas/index.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+import Ninja from '../Ninja'
+
+import './boasvindas.css'
+
+
+export default function BoasVindas() {
+
+    return (
+        <div className="boas-vindas">
+            <h1> Sejam todos bem-vindos </h1>
+            <h2> React na veiaaa!! </h2>
+            <Ninja />
+            <Ninja />
+            <Ninja />
+            <Ninja />
+            <Ninja />
+            <Ninja />
+        </div>
+    );
+}
---
+++ src/components/Ninja/index.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+import './ninja.css'
+
+export default function Ninja() {
+
+    return (
+        <h3 className="sangue-nos-oio">
+            Bora seja Ninjaaaa!
+        </h3>
+    )
+}   
